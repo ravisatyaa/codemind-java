f0be74e95e544da98dd100720dcaac83651e12f3
ravisatyaa <132639641+ravisatyaa@users.noreply.github.com>
2023-08-25 12:00:57 +0530

    import java.util.Scanner; public class l{     public static void main(String[] args){         Scanner s = new Scanner(System.in);         int a = s.nextInt();
            if(a==1){
                System.out.println(0);
            }
            else if(a==2){
                System.out.println(1);
        }
            
            
            else if(a>2){
                System.out.print(2);
            }
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class l{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        
+        
+        if(a==1){
+            System.out.println(0);
+        }
+        else if(a==2){
+            System.out.println(1);
+    }
+        
+        
+        else if(a>2){
+            System.out.print(2);
+        }
+    }
+}
